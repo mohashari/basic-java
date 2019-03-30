7fa2ab22a3d07163138bed577d3e78d4dac7af38
MOH ASHARI MUKLIS <muhashari64@gmail.com>
2019-03-30 22:34:40 +0700

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+
+    public static void main(String[] args) {
+        //data types
+        // primitive data types
+        String name;
+        int angka;
+        double angkaBulat;
+        boolean isActived;
+
+
+//        name = "muklis";
+        angka = 2;
+        angkaBulat = 3.0;
+        isActived = true;
+
+        System.out.println("PRIMITIVE DATA TYPE"
+                + name+" "
+                + angka+ " "
+                + angkaBulat+ " "
+                + isActived);
+
+
+    }
+}
