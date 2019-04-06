3d999ebc483824e7c439a62b351dcb18a085c0cc
MOH ASHARI MUKLIS <muhashari64@gmail.com>
2019-04-06 23:25:27 +0700

    commit
    basic class and object

--- src/Main.java
+++ src/Main.java
@@ -6,47 +6,42 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        //Arrays
-        //default definition
-        //type[] identifier = new type[length];
 
+        // make object class
+        Users users = new Users();
 
-        String[] names = new String[3];
+        Users users1 = new Users("nnn",34);
 
-        // set value arrays
-        names[0] = "muklis";
-        names[1] = "avi";
-        names[2] = "mustofa";
-
-        //declaration arrays
-        String[] tools = {"ss", "ss"};
-
-
-        System.out.println("Arrays: " + names);
-
-        //for loop default
-        for (int i = 0; i < names.length; i++) {
-            System.out.println("name:   " + names[i]);
-
-        }
-
-        //for each
-        for (String tool : tools) {
-            System.out.println("Tool  :    " + tool);
-        }
-
-
-        //java 8 lambda expression
-
-        List<String> animals = new ArrayList<>();
-        animals.add("kanguru");
-        animals.add("cat");
-        animals.add("bird");
-
-        animals.forEach(animal -> {
-            System.out.println("Animal: " + animal);
-        });
+        users.printName("alex");
 
 
     }
 }
+
+
+//sample class and object
+
+class Users {
+
+    // constractor
+    Users() {
+
+    }
+
+    Users(String name, Integer age) {
+        System.out.println(name + age);
+    }
+
+    // state class
+    String name;
+    String age;
+    String address;
+
+
+    // behavior class
+    void printName(String name) {
+        System.out.println(name);
+    }
+
+}
+
